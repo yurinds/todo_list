df5d0e85dfe3c96a83d6015c7a9deb725318698e
Dmitrii Iurin <dimon07710@yandex.ru>
2019-12-17 22:45:00 +0300

    add rendering users tasks on the page

---
+++ application.js
@@ -0,0 +1,84 @@
+const tasks = [
+  {
+    _id: "5d2ca9e2e03d40b326596aa7",
+    completed: true,
+    body:
+      "Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n",
+    title: "Eu ea incididunt sunt consectetur fugiat non."
+  },
+  {
+    _id: "5d2ca9e29c8a94095c1288e0",
+    completed: false,
+    body:
+      "Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n",
+    title:
+      "Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum."
+  },
+  {
+    _id: "5d2ca9e2e03d40b3232496aa7",
+    completed: true,
+    body:
+      "Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n",
+    title: "Eu ea incididunt sunt consectetur fugiat non."
+  },
+  {
+    _id: "5d2ca9e29c8a94095564788e0",
+    completed: false,
+    body:
+      "Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n",
+    title:
+      "Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum."
+  }
+];
+
+(function(arrOfTasks) {
+  const objOfTasks = arrOfTasks.reduce((accum, task) => {
+    accum[task._id] = task;
+    return accum;
+  }, {});
+
+  // Elements UI
+  const listOfTasks = document.querySelector(".row .list-group");
+
+  renderAllTasks(objOfTasks);
+
+  function renderAllTasks(tasksList) {
+    if (!tasksList) return;
+
+    const fragment = document.createDocumentFragment();
+    Object.values(tasksList).forEach(task => {
+      const li = listItemTemplate(task);
+      fragment.appendChild(li);
+    });
+
+    listOfTasks.appendChild(fragment);
+  }
+
+  function listItemTemplate({ _id, body, title } = {}) {
+    const li = document.createElement("li");
+    li.classList.add(
+      "list-group-item",
+      "d-flex",
+      "align-items-center",
+      "flex-wrap",
+      "mt-2"
+    );
+
+    const span = document.createElement("span");
+    span.textContent = title;
+
+    const button = document.createElement("button");
+    button.classList.add("btn", "btn-danger", "ml-auto", "delete-btn");
+    button.textContent = "Delete";
+
+    const paragraph = document.createElement("p");
+    paragraph.classList.add("mt-2", "w-100");
+    paragraph.textContent = body;
+
+    li.appendChild(span);
+    li.appendChild(button);
+    li.appendChild(paragraph);
+
+    return li;
+  }
+})(tasks);
